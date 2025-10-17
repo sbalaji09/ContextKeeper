3afa607e93ab6ec2eb4cbf7ac8b3d03ddf40d3f2
Siddharth Balaji <siddharthbalaji6@gmail.com>
2025-10-17 00:41:26 -0700

    small fix to error of loading app.js

--- frontend/dashboard/context-keeper/src/App.js
+++ frontend/dashboard/context-keeper/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
-import SignUp from "./Signup";
+import SignUp from "./SignUp";
 import Dashboard from "./Dashboard";
 
 function App() {
